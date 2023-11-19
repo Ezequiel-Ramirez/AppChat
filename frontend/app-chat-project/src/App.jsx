5a2b14c144ecf4d3e0b83d3f2244015dca8a7874
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2023-11-19 10:52:10 -0300

    Update package.json and App.jsx files

--- frontend/app-chat-project/src/App.jsx
+++ frontend/app-chat-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Container, Card, Icon, Form, Button } from 'semantic-ui-react'
+import { Container, Card, Form, Button } from 'semantic-ui-react'
 
 import io from 'socket.io-client'
 import Chat from './Chat'
@@ -17,37 +17,39 @@ socket.on('connect', () => {
 function App() {
   const [userName, setUserName] = useState('')
   const [room, setRoom] = useState('')
+  const [showChat, setShowChat] = useState(false)
 
   const joinRoom = () => {
     if (userName !== '' && room !== '') {
       socket.emit('join_room', room)
+      setShowChat(true)
     }
   }
 
   return (
     <Container>
-      <Card fluid>
-        <Card.Content header='Unirme al chat' />
-        <Card.Content>
-          <Form>
-            <Form.Field>
-              <label>Nombre</label>
-              <input type="text" placeholder="Name..." onChange={(e) => { setUserName(e.target.value) }} />
-            </Form.Field>
-            <Form.Field>
-              <label>Sala:</label>
-              <input type="text" placeholder="Room..." onChange={(e) => { setRoom(e.target.value) }} />
-            </Form.Field>
-            <Button onClick={joinRoom}>Join A Room</Button>
-          </Form>
-        </Card.Content>
-        <Card.Content extra>
-          <Icon name='user' />4 Friends
-        </Card.Content>
-      </Card>
-      <Container>
-        <Chat socket={socket} username={userName} room={room} />
-      </Container>
+      {!showChat ? (
+        <Card fluid>
+          <Card.Content header='Unirme al chat' />
+          <Card.Content>
+            <Form>
+              <Form.Field>
+                <label>Nombre</label>
+                <input type="text" placeholder="Name..." onChange={(e) => { setUserName(e.target.value) }} />
+              </Form.Field>
+              <Form.Field>
+                <label>Sala:</label>
+                <input type="text" placeholder="Room..." onChange={(e) => { setRoom(e.target.value) }} />
+              </Form.Field>
+              <Button onClick={joinRoom}>Join A Room</Button>
+            </Form>
+          </Card.Content>
+        </Card>
+      ) : (
+        <Container>
+          <Chat socket={socket} username={userName} room={room} />
+        </Container>
+      )}
     </Container>
   )
 }
--- frontend/app-chat-project/src/Chat.jsx
+++ frontend/app-chat-project/src/Chat.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-import { Container, Card, Form, Input } from 'semantic-ui-react'
+import { Container, Card, Form, Input, Message } from 'semantic-ui-react'
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 const Chat = ({ socket, username, room }) => {
 
     const [currentMessage, setCurrentMessage] = useState("");
+    const [messageList, setMessageList] = useState([]);
 
     const sendMessage = async () => {
         if (username !== "" && currentMessage !== "") {
@@ -16,43 +18,65 @@ const Chat = ({ socket, username, room }) => {
 
             }
             await socket.emit("send_message", messageData);
+            setMessageList((list) => [...list, messageData]);
+            setCurrentMessage("");
         }
     }
 
     useEffect(() => {
-        socket.on("receive_message", (data) => {
-            console.log(data);
-        });
+        const messageHandler = (data) => {
+            setMessageList((list) => [...list, data]);
+        };
+        socket.on("receive_message", messageHandler);
+        return () => socket.off("receive_message", messageHandler);
     }, [socket]);
-
-
-
 
     return (
         <Container>
             <Card fluid>
-                <Card.Content header='Chat en vivo' />
-                <Card.Content>
-                    Mensajes
-                </Card.Content>
-                <Card.Content >
+                <Card.Content header={`Chat en vivo - Sala: ${room}`} />
+                <ScrollToBottom className="messages">
+                    <Card.Content style={{ minHeight: '400px', padding: '5px' }}>
+                        {
+                            messageList.map((val, key) => {
+                                return (
+                                    <Message
+                                        key={key}
+                                        style={{ textAlign: val.author === username ? 'right' : 'left' }}
+                                        success={val.author === username ? true : false}
+                                        info={val.author === username ? false : true}
+                                    >
+                                        <Message.Header>{val.message}</Message.Header>
+                                        <p>{val.author} - {val.time}</p>
+                                    </Message>
+                                );
+                            })
+                        }
+                    </Card.Content>
+                </ScrollToBottom>
+                <Card.Content extra >
                     <Form>
                         <Form.Field>
-                            <Input
-                                action={{
-                                    color: 'teal',
-                                    labelPosition: 'right',
-                                    icon: 'send',
-                                    content: 'Enviar',
-                                    onClick: sendMessage
-                                }}
-                                placeholder='Escribe un mensaje...'
-                                onChange={(e) => {
-                                    setCurrentMessage(e.target.value);
-                                }
-                                }
-                            />
-
+                            <div className="ui action input">
+                                <Input
+                                    type="text"
+                                    placeholder='Escribe un mensaje...'
+                                    value={currentMessage}
+                                    onChange={(e) => {
+                                        setCurrentMessage(e.target.value);
+                                    }
+                                    }
+                                    onKeyPress={(e) => {
+                                        if (e.key === 'Enter') {
+                                            sendMessage();
+                                        }
+                                    }}
+                                />
+                                <button type="button" className="ui teal icon right labeled button" onClick={sendMessage}>
+                                    <i aria-hidden="true" className="send icon"></i>
+                                    Enviar
+                                </button>
+                            </div>
                         </Form.Field>
                     </Form>
                 </Card.Content>
